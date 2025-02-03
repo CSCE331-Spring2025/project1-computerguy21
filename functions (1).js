9597110d6e9fef11d600c5e02b4a24b98ac4e010
Dave Codez-Laptop <david.armijo101@gmail.com>
2025-02-03 00:51:09 -0600

    Final Draft v1.1: Modified Styling to accommodate the icons

---
+++ functions (1).js
@@ -0,0 +1,50 @@
+var num = 0; // global variable
+
+function toggleStyleSheet(){
+    // Task 1
+    // Steps
+    // 1 (a) Get style element by ID (hint: getElementById)
+    var element = document.getElementById("mainStyleSheet");
+    
+    // 1 (b) Check the current stylesheet file name. (hint: element.getAttribute)
+    var styleAttribute = element.getAttribute("href");
+   
+    // 1 (c) Determine new stylesheet file name
+    var newss
+    if(styleAttribute == "style1.css"){
+        newss = "style2.css";
+    }
+    else{
+        newss = "style1.css";
+    }
+
+    // 1 (d) replace stylesheet with new stylesheet (hint: element.setAttribute)
+    element.setAttribute("href", newss);
+
+
+    // TASK 2
+    // 2 (d) For persistence when page is refreshed. save new stylesheet name to localStorage
+    // specify what you're changing -maybe paul
+    // hint: localStorage.setItem(name, value)
+    localStorage.setItem("stylesheet",newss)
+}
+
+
+window.onload = function(){
+    // TASK 2
+    // TODO: Make the last div color persist even when someone refreshes the page.
+
+    // Steps
+    // 2 (a) get stylesheet name from local storage hint: localStorage.getItem(name)
+    var styleStorage = localStorage.getItem("stylesheet");
+    
+    // 2 (b) get html style element by ID
+    var styleElement = document.getElementById("mainStyleSheet");
+
+    // 2 (c) replace href attribute of html element.
+    if(styleStorage){
+        styleElement.setAttribute("href", styleStorage);
+    }    
+}
+
+
